c3c0fb614d1b73bde63f39973d823e78f47ede82
Datist Pham <78075406+datistpham@users.noreply.github.com>
2023-04-24 08:50:27 +0700

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -10,9 +10,9 @@
 const mysql= require("mysql2/promise")
 
 const connection= mysql.createPool({
-    host: "sql479.main-hosting.eu",
+    host: "151.106.124.151",
     user: "u291392387_root10",
-    password: "Giangvippro1!",
+    password: "GIangvippro1!",
     database: "u291392387_library_system",
 })
 
